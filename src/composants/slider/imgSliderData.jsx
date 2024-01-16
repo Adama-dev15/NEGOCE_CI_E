ba898c12ab6bf5399ad95ebeb148b0b40619ca76
Traoré Adama <traoreadamakoleba@gmail.com>
2024-01-16 11:24:07 +0000

    add

---
+++ src/composants/slider/imgSliderData.jsx
@@ -0,0 +1,11 @@
+const ImgData = [
+    {
+        imgName: "images-slider/eco1.jpg",
+    },
+    {
+        imgName: "images-slider/eco2.jpg",
+    },
+    {
+        imgName: "images-slider/eco3.jpg",
+    },
+]
